ad0103ae3517c9df33515076b8668fc0165751e2
Akshansh Dhing <akshansh.dhing@bookmyshow.com>
2022-10-12 12:42:32 +0530

    Added some common library support.

--- feature-one/src/main/java/com/bookmyshow/feature_one/FeatureOneActivity.kt
+++ feature-one/src/main/java/com/bookmyshow/feature_one/FeatureOneActivity.kt
@@ -1,10 +1,14 @@
 package com.bookmyshow.feature_one
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bookmyshow.feature_one.di.FeatureOneDaggerProvider
+import com.bookmyshow.feature_one.viewmodel.FeatureOneViewModel
 
 class FeatureOneActivity : AppCompatActivity() {
+
+    private val viewModel: FeatureOneViewModel by viewModels<FeatureOneViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ feature-one/src/main/java/com/bookmyshow/feature_one/viewmodel/FeatureOneViewModel.kt
@@ -0,0 +1,10 @@
+package com.bookmyshow.feature_one.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Akshansh Dhing on 12/10/22.
+ */
+class FeatureOneViewModel : ViewModel() {
+
+}
